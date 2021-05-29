2fb909f7b1a76417e42ee0b2a467cd6f0a7dcb6d
Ravi Gupta <ravi.gupta@yahoo.in>
2021-05-29 08:22:04 +0000

    Initial Commit

---
+++ src/test/java/com/learning/msapigateway/MsApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learning.msapigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
